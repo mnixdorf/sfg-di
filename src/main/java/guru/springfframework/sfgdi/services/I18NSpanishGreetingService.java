11586cfd9cc2da788c330615504d46c19c9f48d6
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2022-12-30 11:43:37 +0100

    default profile implementation added

--- src/main/java/guru/springfframework/sfgdi/services/I18NSpanishGreetingService.java
+++ src/main/java/guru/springfframework/sfgdi/services/I18NSpanishGreetingService.java
@@ -2,7 +2,8 @@ package guru.springfframework.sfgdi.services;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-@Profile("ES")
+
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18NSpanishGreetingService implements GreetingService{
 
