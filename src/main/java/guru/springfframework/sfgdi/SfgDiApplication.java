ab25292e2954f55186b5c57f06d15bb89f1a504e
mnixdorf <35327832+mnixdorf@users.noreply.github.com>
2023-01-02 14:20:57 +0100

    greetingserviceconfig added

--- src/main/java/guru/springfframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springfframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,8 @@ import guru.springfframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = {"guru.springfframework.sfgdi", "com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
---
+++ src/main/java/guru/springfframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package guru.springfframework.sfgdi.config;
+
+import guru.springfframework.sfgdi.services.ConstructorGreetingService;
+import guru.springfframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springfframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
